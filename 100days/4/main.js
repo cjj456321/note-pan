f26947b7c6eff389db6832684d6d0310ca516b7d
jack.pan <jack.pan@brightcells.com>
2016-03-29 19:23:52 +0800

    js button click value changed from A to D

--- 100days/4/main.js
+++ 100days/4/main.js
@@ -1,60 +1,57 @@
-
 // left selection hidden and show 
-var friends = document.getElementsByTagName('h2')[0];
+var friends = document.getElementsByTagName('h2')[0]
 function shifticon () {
-	var arrow = document.getElementById('icon'),
-		friends = document.querySelector('.friends');
-	if(arrow.className == 'arrow-bottom'){
-		arrow.className = 'arrow-right';
-		friends.style.cssText = 'opacity:0';
-	}else{
-		arrow.className = 'arrow-bottom';
-		friends.style.cssText = 'opacity:1';
-	}
+  var arrow = document.getElementById('icon'),
+    friends = document.querySelector('.friends')
+  if (arrow.className == 'arrow-bottom') {
+    arrow.className = 'arrow-right'
+    friends.style.cssText = 'opacity:0'
+  } else {
+    arrow.className = 'arrow-bottom'
+    friends.style.cssText = 'opacity:1'
+  }
 }
-friends.addEventListener('click',shifticon,false);
-
+friends.addEventListener('click', shifticon, false)
 
 // send text
 var btn = document.querySelector('.right button'),
-	html = '';
+  html = ''
 function sendText () {
-	var hash = location.hash,
-		text = document.querySelector('.right input'),
-		 msg = text.value;
+  var hash = location.hash,
+    text = document.querySelector('.right input'),
+    msg = text.value
 
-	if(msg === ''){
-		alert('please write a word');
-		return false;
-	}
-	if(hash == ''){
-		alert('please select a friend!');
-		return false;
-	}
-	var chatContent = document.querySelector('.right > .top'),
- 		date = new Date(),
- 		time = date.getHours() + ':' + date.getSeconds();
-	if(hash === '#jack'){
-		var jackMsg = localStorage.jack;
-		if(jackMsg){
-			text.value='';
-			localStorage.jack = jackMsg + ';' + msg;
-			html += '<p>' + time + '>>> ' + msg+ '</p>'
-		}else{
-			localStorage.jack = text.value;
-			html += '<p>' + time + '>>> ' + msg+ '</p>'
-			text.value = '';
-		}
-		chatContent.innerHTML = html;
+  if (msg === '') {
+    alert('please write a word')
+    return false
+  }
+  if (hash == '') {
+    alert('please select a friend!')
+    return false
+  }
+  var chatContent = document.querySelector('.right > .top'),
+    date = new Date(),
+    time = date.getHours() + ':' + date.getSeconds()
+  if (hash === '#jack') {
+    var jackMsg = localStorage.jack
+    if (jackMsg) {
+      text.value = ''
+      localStorage.jack = jackMsg + ';' + msg
+      html += '<p>' + time + '>>> ' + msg + '</p>'
+    } else {
+      localStorage.jack = text.value
+      html += '<p>' + time + '>>> ' + msg + '</p>'
+      text.value = ''
+    }
+    chatContent.innerHTML = html
+  }
 
-	}
+  if (hash === '#rose') {
+    alert('rose')
+  }
 
-	if(hash === '#rose'){
-		alert('rose')
-	}
-
-	if(hash === '#peter'){
-		alert('peter');
-	}
+  if (hash === '#peter') {
+    alert('peter')
+  }
 }
-btn.addEventListener('click',sendText, false);
+btn.addEventListener('click', sendText, false)
