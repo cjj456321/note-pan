8902cd3d9c67a6346602618cc5f48e4fa8b32cc1
jack.pan <jack.pan@brightcells.com>
2016-03-31 15:26:22 +0800

    add anim mouse round

--- configs/utils.js
+++ configs/utils.js
@@ -1,7 +1,7 @@
 // 获取鼠标在某个元素上的位置
 function getMousePos (ele) {
-  let mouse = {x: null, y: null}
-  mouse.addEventListener('mousemove', function (e) {
+  let mouse = {x: 0, y: 0}
+  ele.addEventListener('mousemove', function (e) {
     mouse.x = e.pageX - ele.offsetLeft
     mouse.y = e.pageY - ele.offsetTop
   })
